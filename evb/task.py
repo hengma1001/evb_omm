157c051cdf74ec2cdf98bf45e9383d5b99ab5385
Heng Ma <maheng1991@hotmail.com>
2023-02-24 22:17:06 +0000

    added multi host support

--- evb/task.py
+++ evb/task.py
@@ -1,7 +1,6 @@
 import os
-import GPUtil
-import subprocess
 import logging
+import subprocess
 from typing import List, Dict, Tuple, Set
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
@@ -12,40 +11,59 @@ class InsufficientResources(BaseException):
     pass
 
 class GPUManager:
-    def __init__(self, maxLoad=.2, maxMemory=.8):
-        self.maxLoad = maxLoad
-        self.maxMemory = maxMemory
-        self.gpus = GPUtil.getGPUs()
+    def __init__(self, hostfile=None):
+        self.nranks_per_node = int(subprocess.check_output("nvidia-smi -L | wc -l", shell=True))
+        self.hosts = self.get_node_list(hostfile=hostfile)
+        self.n_gpus = self.nranks_per_node * len(self.hosts)
+
+    @classmethod
+    def get_node_list(cls, hostfile=None):
+        if hostfile is None:
+            hostfile = os.environ["PBS_NODEFILE"]
+            print(hostfile)
+        with open(hostfile, 'r') as fp:
+            data = fp.read()
+        splitter = ',' if ',' in data else None
+        return [node_id for node_id in data.split(splitter)]
 
     def request(self, num_gpus:int) -> Set[int]:
-        try: 
-            request_gpus = GPUtil.getAvailable(self.gpus, limit=num_gpus,
-                    maxLoad=self.maxLoad, maxMemory=self.maxMemory)
-        except IndexError: 
-            raise InsufficientResources("Not enough resource available for the request. ")
-        return request_gpus
+        if self.n_gpus > num_gpus:
+            return list(range(num_gpus))
+        else: 
+            return list(range(self.n_gpus))
+
+    def loc_gpu(self, gpu_id): 
+        host = self.hosts[ gpu_id // self.nranks_per_node ]
+        gpu_id = gpu_id % self.nranks_per_node
+        return host, gpu_id
 
 
 class RunTemplate:
     @staticmethod
     def _env_str(envs):
-        envstrs = (f'export {var}="{val}" && ' for var, val in envs.items())
+        envstrs = (f"export {var}='{val}' && " for var, val in envs.items())
         return " ".join(envstrs)
 
     @staticmethod
     def render(
         command_line: str,
+        cwd=None,
         gpu_ids=None,
         envs_dict=None,
+        host=None
     ):
 
         if envs_dict is None:
             envs_dict = {}
         if gpu_ids:
-            envs_dict["CUDA_VISIBLE_DEVICES"] = ",".join(str(id) for id in gpu_ids)
+            envs_dict["CUDA_VISIBLE_DEVICES"] = ','.join(str(id) for id in gpu_ids)
         envs = RunTemplate._env_str(envs_dict)
-
-        return f"{envs} {command_line}"
+        cmd = f"{envs} {command_line}"
+        if cwd:
+            cmd =  f"cd {cwd} && {cmd}"
+        if host: 
+            cmd = f'ssh {host} "{cmd}"'
+        return cmd
 
 
 class Run:
@@ -57,6 +75,7 @@ class Run:
         output_file,
         gpu_ids=None,
         cwd=None,
+        host=None,
         envs_dict: Dict[str, str] = None,
     ):
         self.gpu_ids = gpu_ids
@@ -64,7 +83,9 @@ class Run:
 
         command = RunTemplate.render(
             command_line=cmd_line,
+            cwd=cwd,
             gpu_ids=gpu_ids,
+            host=host,
             envs_dict=envs_dict,
         )
 
@@ -72,7 +93,6 @@ class Run:
         self.process = subprocess.Popen(
             command,
             shell=True,
-            cwd=cwd,
             stdout=self.outfile,
             stderr=subprocess.STDOUT,
             preexec_fn=os.setsid
@@ -91,7 +111,7 @@ class Run:
 
 if __name__ == "__main__":
     gpu_manager = GPUManager()
-    print(gpu_manager.gpus)
+    print(gpu_manager.hosts)
     gpus = gpu_manager.request(num_gpus=1)
     print(gpus)
     gpus = gpu_manager.request(num_gpus=2)
@@ -104,5 +124,7 @@ if __name__ == "__main__":
     print(gpus)
     gpus = gpu_manager.request(num_gpus=6)
     print(gpus)
-    gpus = gpu_manager.request(num_gpus=7)
+    gpus = gpu_manager.request(num_gpus=9)
     print(gpus)
+    for gpu in gpus: 
+        print(gpu_manager.loc_gpu(gpu))
